46bc4972e2ba659ab5aae85e56be453b5941abf4
dzurrotuumi13 <161183181+dzurrotuumi13@users.noreply.github.com>
2024-11-04 17:00:10 +0700

    Create SI_RegPagi_23176020-Latihan 26-Waktu Saat Ini.java

---
+++ SI_RegPagi_23176020-Latihan 26-Waktu Saat Ini.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pkg23176020pbo5;
+
+/**
+ *
+ * @author Asus
+ * Nama : Dzurrotu Umi Asyfiya
+ * NIM : 23176020
+ * Prodi : Sistem Informasi
+ * Deskripsi Program: Rangkaian kode berikut berisi mengenai Waktu saat ini
+ */
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public class WaktuSaatIni {
+    public static void main(String[] args) {
+        LocalDateTime now = LocalDateTime.now();
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("EEEE, dd MMM yyyy HH:mm:ss", Locale.forLanguageTag("id-ID"));
+        String formattedDateTime = now.format(formatter);
+
+        System.out.println("Hari ini adalah hari " + formattedDateTime);
+    }
+}
